2ee365ade364e3d14df275b9d31aa39e197f4df1
joserubemneto <joserubemn31@gmail.com>
2023-01-01 23:55:57 +0000

    chore: add project setup

---
+++ readme.md
@@ -0,0 +1,60 @@
+# rubemneto.com
+
+## Technology stack
+
+- **Styling:** [Stitches](https://stitches.dev/)
+- **Front-end:** [React](https://reactjs.org/)
+- **Deployment:** [Vercel](https://vercel.com/)
+
+## Running locally
+
+1. Clone this repo:
+
+```sh
+$ git clone https://github.com/joserubemneto/rubemneto.com.git
+```
+
+2. Then go to the project's folder:
+
+```sh
+cd rubemneto.com
+```
+
+3. Install all dependencies:
+
+```sh
+npm install
+```
+
+4. Run locally:
+
+```sh
+npm run dev
+```
+
+## File structure
+
+The basic file structure for the project is organized in the following way:
+
+```
+.
+|-- components
+|-- pages
+`-- public
+```
+
+### [components](https://github.com/zenorocha/zenorocha.com/tree/master/components)
+
+Here you'll find reusable blocks of React components.
+
+### [pages](https://github.com/zenorocha/zenorocha.com/tree/master/pages)
+
+Here you'll find all the main pages of the site.
+
+### [public](https://github.com/zenorocha/zenorocha.com/blob/master/public)
+
+Here you'll find all the images and font files.
+
+## License
+
+to be filled
---
+++ stitches.config.ts
@@ -0,0 +1,138 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {
+      yellow: '#ffff80',
+      pink: '#ff80bf',
+      purple: '#9580ff',
+      red: '#ff9580',
+      orange: '#ffca80',
+      green: '#8aff80',
+      cyan: '#80ffea',
+      white: '#ffffff',
+      primary: '#f2f2f2',
+      secondary: '#8f9ba8',
+      background: '#08070b',
+      hover: '#212024',
+      black: 'rgba(255, 255, 255, 0.05)',
+    },
+    fonts: {
+      body: 'Biotif, sans-serif',
+      code: 'Fira Code, monospace',
+      heading: 'Neuzeit Grotesk Bold, sans-serif',
+    },
+    fontSizes: {
+      xxs: '0.625rem',
+      xs: '0.75rem',
+      sm: '0.875rem',
+      md: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '4xl': '2rem',
+      '5xl': '2.25rem',
+      '6xl': '3rem',
+      '7xl': '4rem',
+      '8xl': '4.5rem',
+      '9xl': '6rem',
+    },
+    space: {
+      1: '0.25rem',
+      2: '0.5rem',
+      3: '0.75rem',
+      4: '1rem',
+      5: '1.25rem',
+      6: '1.5rem',
+      7: '1.75rem',
+      8: '2rem',
+      10: '2.5rem',
+      12: '3rem',
+      16: '4rem',
+      20: '5rem',
+      40: '10rem',
+      64: '16rem',
+      80: '20rem',
+    },
+    transitions: {
+      duration: '0.2s',
+    },
+    radii: {
+      md: '8px',
+    },
+  },
+  media: {
+    bp1: '(min-width: 425px)',
+    bp2: '(min-width: 760px)',
+    bp3: '(max-width: 780px)',
+    bp4: '(max-width: 1024px)',
+  },
+})
+
+const globalStyles = globalCss({
+  '*': {
+    margin: '0',
+    padding: '0',
+    boxSizing: 'border-box',
+  },
+  'html, body': {
+    WebkitFontSmoothing: 'antialiased',
+    background: '$background',
+    fontFamily: '$body',
+  },
+  '@font-face': [
+    {
+      fontFamily: 'Neuzeit Grotesk Bold',
+      src: `url("/static/font/NeuzeitGrotesk-Bold.woff2") format("woff2"),
+        url("/static/font/NeuzeitGrotesk-Bold.woff") format("woff")`,
+      fontWeight: 'normal',
+      fontStyle: 'normal',
+    },
+    {
+      fontFamily: 'Fira Code',
+      src: `url("/static/font/FiraCode-Regular.woff2") format("woff2"),
+        url("/static/font/FiraCode-Regular.woff") format("woff")`,
+      fontWeight: 'normal',
+      fontStyle: 'normal',
+    },
+    {
+      fontFamily: 'Biotif',
+      src: `url("/static/font/Biotif-Bold.woff2") format("woff2"),
+        url("/static/font/Biotif-Bold.woff") format("woff")`,
+      fontWeight: 'bold',
+      fontStyle: 'normal',
+    },
+    {
+      fontFamily: 'Biotif',
+      src: `url("/static/font/Biotif-Book.woff2") format("woff2"),
+        url("/static/font/Biotif-Book.woff") format("woff")`,
+      fontWeight: 500,
+      fontStyle: 'normal',
+    },
+    {
+      fontFamily: 'Biotif',
+      src: `url("/static/font/Biotif-Regular.woff2") format("woff2"),
+        url("/static/font/Biotif-Regular.woff") format("woff")`,
+      fontWeight: 'normal',
+      fontStyle: 'normal',
+    },
+    {
+      fontFamily: 'Biotif',
+      src: `url("/static/font/Biotif-RegularItalic.woff2") format("woff2"),
+        url("/static/font/Biotif-RegularItalic.woff") format("woff")`,
+      fontWeight: 'normal',
+      fontStyle: 'italic',
+    },
+  ],
+})
+
+globalStyles()

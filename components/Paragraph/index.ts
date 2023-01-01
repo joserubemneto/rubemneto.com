e183f384557a7814bab5e0406f52c51e949ed1df
joserubemneto <joserubemn31@gmail.com>
2023-01-01 23:57:10 +0000

    feat: create Paragraph component

---
+++ components/Paragraph/index.ts
@@ -0,0 +1,10 @@
+import { styled } from '../../stitches.config'
+
+export const Paragraph = styled('p', {
+  color: '$secondary',
+
+  strong: {
+    color: '$primary',
+    fontWeight: 500,
+  },
+})

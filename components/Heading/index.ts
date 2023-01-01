772556aec1828a7201fff8c143241e03ac9afaaa
joserubemneto <joserubemn31@gmail.com>
2023-01-01 23:56:34 +0000

    feat: create Heading component

---
+++ components/Heading/index.ts
@@ -0,0 +1,20 @@
+import { styled } from '../../stitches.config'
+
+export const Heading = styled('h2', {
+  color: '$primary',
+  variants: {
+    as: {
+      h1: {
+        fontFamily: '$heading',
+        fontSize: '$6xl',
+        lineHeight: '50px',
+      },
+      h2: {
+        fontSize: '$2xl',
+      },
+      h3: {
+        fontSize: '$lg',
+      },
+    },
+  },
+})

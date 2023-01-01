41a0ce9dca52d1b72a3c0234d73ebe3cf984afc8
joserubemneto <joserubemn31@gmail.com>
2023-01-01 23:56:51 +0000

    feat: create Navbar component

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,75 @@
+import { styled } from '../../stitches.config'
+import { useRouter } from 'next/router'
+import { BaseNavLink } from '../Link'
+
+type NavbarProps = {
+  links: {
+    description: string
+    href: string
+  }[]
+}
+
+const Header = styled('header', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  color: 'white',
+  fontSize: '$xs',
+  height: '60px',
+  width: '100%',
+})
+
+const Logo = styled('a', {
+  color: '$white',
+  textDecoration: 'none',
+  fontWeight: 700,
+  fontSize: '$4xl',
+  fontFamily: '$heading',
+  transition: 'opacity $duration ease-in-out',
+
+  '&:hover, &:focus': {
+    opacity: '0.8',
+  },
+})
+
+const Nav = styled('nav', {
+  textAlign: 'center',
+
+  ul: {
+    display: 'flex',
+    gap: '$5',
+    listStyle: 'none',
+  },
+})
+
+const NavLink = styled(BaseNavLink, {
+  variants: {
+    isActive: {
+      true: {
+        color: '$primary',
+      },
+    },
+  },
+})
+
+export function Navbar({ links }: NavbarProps) {
+  const { pathname } = useRouter()
+
+  return (
+    <Header>
+      <Logo href="/">R</Logo>
+
+      <Nav>
+        <ul>
+          {links.map(({ description, href }) => (
+            <li key={href}>
+              <NavLink href={href} isActive={pathname === href}>
+                {description}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </Nav>
+    </Header>
+  )
+}

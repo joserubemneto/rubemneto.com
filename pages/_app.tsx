65c5ab41c7100c6da1ad9b80a0635c4867a12523
joserubemneto <joserubemn31@gmail.com>
2023-01-01 23:57:58 +0000

    feat: add app entry point

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import { AppProps } from 'next/app'
+import { Layout } from '../components/Layout'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
